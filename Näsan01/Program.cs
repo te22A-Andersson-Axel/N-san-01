3f46cbee022e3e32256799c7e009d2914718d85b
Axel Andersson <axel.andersson@elev.ga.ntig.se>
2024-11-12 11:08:20 +0100

    ASD

---
+++ Näsan01/Program.cs
@@ -0,0 +1,15 @@
+﻿if (6 >= 3)
+{
+    Console.WriteLine("Hello world!");
+}
+
+Console.WriteLine("What is your name?: ");
+string name = Console.ReadLine();
+
+Console.WriteLine("What is the password?: ");
+string password = Console.ReadLine();
+
+if (name == "kalleanka" && password == "12345")
+{
+    Console.WriteLine("Welcome!");
+}
